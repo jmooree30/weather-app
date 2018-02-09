93d5881b63f6027802cf6b04e21481263e646895
Jacob <jmooree30@gmail.com>
2018-02-09 00:22:11 -0500

    need new background

--- index.js
+++ index.js
@@ -3,21 +3,21 @@ let currentDesc = document.querySelector("#currentDesc");
 let currentLocation = document.querySelector("#currentLocation");
 let currentHumidity = document.querySelector("#currentHumidity");
 let currentIcon = document.querySelector("#currentIcon");
-let oneIcon = document.querySelector("#oneIcon")
+let oneIcon = document.querySelector("#oneIcon");
 let oneTemp = document.querySelector("#oneTemp");
-let oneDesc = document.querySelector("#oneDesc")
-let twoIcon = document.querySelector("#twoIcon")
+let oneDesc = document.querySelector("#oneDesc");
+let twoIcon = document.querySelector("#twoIcon");
 let twoTemp = document.querySelector("#twoTemp");
-let twoDesc = document.querySelector("#twoDesc")
-let threeIcon = document.querySelector("#threeIcon")
+let twoDesc = document.querySelector("#twoDesc");
+let threeIcon = document.querySelector("#threeIcon");
 let threeTemp = document.querySelector("#threeTemp");
-let threeDesc = document.querySelector("#threeDesc")
-let fourIcon = document.querySelector("#fourIcon")
+let threeDesc = document.querySelector("#threeDesc");
+let fourIcon = document.querySelector("#fourIcon");
 let fourTemp = document.querySelector("#fourTemp");
-let fourDesc = document.querySelector("#fourDesc")
-let fiveIcon = document.querySelector("#fiveIcon")
+let fourDesc = document.querySelector("#fourDesc");
+let fiveIcon = document.querySelector("#fiveIcon");
 let fiveTemp = document.querySelector("#fiveTemp");
-let fiveDesc = document.querySelector("#fiveDesc")
+let fiveDesc = document.querySelector("#fiveDesc");
 
 // current weather
 fetch(
@@ -50,40 +50,39 @@ fetch(
   })
   .then(function(response) {
     console.log(response);
-    // day 1 
+    // day 1
     let iconOneCode = response.list[2].weather[0].icon;
     let iconOneUrl = "http://openweathermap.org/img/w/" + iconOneCode + ".png";
     oneTemp.innerHTML = response.list[2].main.temp.toFixed(0) + " degrees";
-    oneIcon.src = iconOneUrl
-    oneDesc.innerHTML = response.list[2].weather[0].description
-
+    oneIcon.src = iconOneUrl;
+    oneDesc.innerHTML = response.list[2].weather[0].description;
     // day 2
     let iconTwoCode = response.list[10].weather[0].icon;
     let iconTwoUrl = "http://openweathermap.org/img/w/" + iconTwoCode + ".png";
     twoTemp.innerHTML = response.list[10].main.temp.toFixed(0) + " degrees";
-    twoIcon.src = iconTwoUrl
-    twoDesc.innerHTML = response.list[10].weather[0].description
-    
+    twoIcon.src = iconTwoUrl;
+    twoDesc.innerHTML = response.list[10].weather[0].description;
     // day 3
     let iconThreeCode = response.list[18].weather[0].icon;
-    let iconThreeUrl = "http://openweathermap.org/img/w/" + iconThreeCode + ".png";
+    let iconThreeUrl =
+      "http://openweathermap.org/img/w/" + iconThreeCode + ".png";
     threeTemp.innerHTML = response.list[18].main.temp.toFixed(0) + " degrees";
-    threeIcon.src = iconThreeUrl
-    threeDesc.innerHTML = response.list[18].weather[0].description
-    
+    threeIcon.src = iconThreeUrl;
+    threeDesc.innerHTML = response.list[18].weather[0].description;
     // day 4
     let iconFourCode = response.list[26].weather[0].icon;
-    let iconFourUrl = "http://openweathermap.org/img/w/" + iconFourCode + ".png";
+    let iconFourUrl =
+      "http://openweathermap.org/img/w/" + iconFourCode + ".png";
     fourTemp.innerHTML = response.list[26].main.temp.toFixed(0) + " degrees";
-    fourIcon.src = iconFourUrl
-    fourDesc.innerHTML = response.list[26].weather[0].description
-
+    fourIcon.src = iconFourUrl;
+    fourDesc.innerHTML = response.list[26].weather[0].description;
     // day 5
     let iconFiveCode = response.list[34].weather[0].icon;
-    let iconFiveUrl = "http://openweathermap.org/img/w/" + iconFiveCode + ".png";
+    let iconFiveUrl =
+      "http://openweathermap.org/img/w/" + iconFiveCode + ".png";
     fiveTemp.innerHTML = response.list[34].main.temp.toFixed(0) + " degrees";
-    fiveIcon.src = iconFiveUrl
-    fiveDesc.innerHTML = response.list[34].weather[0].description
+    fiveIcon.src = iconFiveUrl;
+    fiveDesc.innerHTML = response.list[34].weather[0].description;
   })
   .catch(function(err) {
     console.log(err);
