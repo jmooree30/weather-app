3a0d58c2c534f9ecc76a9a6c680708b79b661f84
Jacob <jmooree30@gmail.com>
2018-02-10 14:22:08 -0500

    form functioning

--- index.js
+++ index.js
@@ -20,6 +20,98 @@ let fiveTemp = document.querySelector("#fiveTemp");
 let fiveDesc = document.querySelector("#fiveDesc");
 let body = document.querySelector("body");
 
+let city = "02117";
+
+document.querySelector("form").addEventListener("submit", function(e) {
+  e.preventDefault();
+  city = document.querySelector("input").value;
+  loader();
+});
+
+// current weather
+function weather() {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?zip=${city},us&units=imperial&APPID=38c7a3bf4046499d8be4a15128e0d64a`,
+    { mode: "cors" }
+  )
+    .then(function(response) {
+      document.querySelector(".loader").style.display = "none";
+      return response.json();
+    })
+    .then(function(response) {
+      let ts = Math.round(new Date().getTime() / 1000) + 86400;
+      if (ts < response.sys.sunset) {
+        body.style.backgroundImage = "url('assets/1.png')";
+      } else {
+        body.style.backgroundImage = "url('assets/2.jpg')";
+      }
+      let iconCode = response.weather[0].icon;
+      let iconUrl = "http://openweathermap.org/img/w/" + iconCode + ".png";
+      currentIcon.src = iconUrl;
+      currentTemp.innerHTML = response.main.temp.toFixed(0) + " F";
+      currentDesc.innerHTML = response.weather[0].description;
+      currentHumidity.innerHTML = "Humidity: " + response.main.humidity;
+      currentLocation.innerHTML = response.name;
+    })
+    .catch(function(err) {
+      alert("Zip code not found. Please enter a valid zip code.");
+    });
+
+  // 5 day forcast
+  fetch(
+    `https://api.openweathermap.org/data/2.5/forecast?zip=${city},us&units=imperial&APPID=38c7a3bf4046499d8be4a15128e0d64a`,
+    { mode: "cors" }
+  )
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(response) {
+      // day 1
+      document.querySelector("#oneDay").innerHTML = dayAbbrv(1);
+      let iconOneCode = response.list[2].weather[0].icon;
+      let iconOneUrl =
+        "http://openweathermap.org/img/w/" + iconOneCode + ".png";
+      oneTemp.innerHTML = response.list[2].main.temp.toFixed(0);
+      oneIcon.src = iconOneUrl;
+      oneDesc.innerHTML = response.list[2].weather[0].description;
+      // day 2
+      document.querySelector("#twoDay").innerHTML = dayAbbrv(2);
+      let iconTwoCode = response.list[10].weather[0].icon;
+      let iconTwoUrl =
+        "http://openweathermap.org/img/w/" + iconTwoCode + ".png";
+      twoTemp.innerHTML = response.list[10].main.temp.toFixed(0);
+      twoIcon.src = iconTwoUrl;
+      twoDesc.innerHTML = response.list[10].weather[0].description;
+      // day 3
+      document.querySelector("#threeDay").innerHTML = dayAbbrv(3);
+      let iconThreeCode = response.list[18].weather[0].icon;
+      let iconThreeUrl =
+        "http://openweathermap.org/img/w/" + iconThreeCode + ".png";
+      threeTemp.innerHTML = response.list[18].main.temp.toFixed(0);
+      threeIcon.src = iconThreeUrl;
+      threeDesc.innerHTML = response.list[18].weather[0].description;
+      // day 4
+      document.querySelector("#fourDay").innerHTML = dayAbbrv(4);
+      let iconFourCode = response.list[26].weather[0].icon;
+      let iconFourUrl =
+        "http://openweathermap.org/img/w/" + iconFourCode + ".png";
+      fourTemp.innerHTML = response.list[26].main.temp.toFixed(0);
+      fourIcon.src = iconFourUrl;
+      fourDesc.innerHTML = response.list[26].weather[0].description;
+      // day 5
+      document.querySelector("#fiveDay").innerHTML = dayAbbrv(5);
+      let iconFiveCode = response.list[34].weather[0].icon;
+      let iconFiveUrl =
+        "http://openweathermap.org/img/w/" + iconFiveCode + ".png";
+      fiveTemp.innerHTML = response.list[34].main.temp.toFixed(0) + " degrees";
+      fiveIcon.src = iconFiveUrl;
+      fiveDesc.innerHTML = response.list[34].weather[0].description;
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
+}
+
 function dayAbbrv(day) {
   var d = new Date();
   var n = d.getDay();
@@ -52,84 +144,12 @@ function dayAbbrv(day) {
     case 6:
       day = "Sat";
   }
-  return day
+  return day;
 }
 
-// current weather
-fetch(
-  "https://api.openweathermap.org/data/2.5/weather?zip=02119,us&units=imperial&APPID=38c7a3bf4046499d8be4a15128e0d64a",
-  { mode: "cors" }
-)
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(response) {
-    let ts = Math.round(new Date().getTime() / 1000) + 86400;
-    if (ts < response.sys.sunset) {
-      body.style.backgroundImage = "url('assets/1.png')";
-    } else {
-      body.style.backgroundImage = "url('assets/2.jpg')";
-    }
-    let iconCode = response.weather[0].icon;
-    let iconUrl = "http://openweathermap.org/img/w/" + iconCode + ".png";
-    currentIcon.src = iconUrl;
-    currentTemp.innerHTML = response.main.temp.toFixed(0) + " F";
-    currentDesc.innerHTML = response.weather[0].description;
-    currentHumidity.innerHTML = "Humidity: " + response.main.humidity;
-    currentLocation.innerHTML = response.name;
-  })
-  .catch(function(err) {
-    console.log(err);
-  });
+function loader() {
+  document.querySelector(".loader").style.display = "block";
+  weather();
+}
 
-// 5 day forcast
-fetch(
-  "https://api.openweathermap.org/data/2.5/forecast?zip=02119,us&units=imperial&APPID=38c7a3bf4046499d8be4a15128e0d64a",
-  { mode: "cors" }
-)
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(response) {
-    // day 1
-    document.querySelector("#oneDay").innerHTML = dayAbbrv(1);
-    let iconOneCode = response.list[2].weather[0].icon;
-    let iconOneUrl = "http://openweathermap.org/img/w/" + iconOneCode + ".png";
-    oneTemp.innerHTML = response.list[2].main.temp.toFixed(0);
-    oneIcon.src = iconOneUrl;
-    oneDesc.innerHTML = response.list[2].weather[0].description;
-    // day 2
-    document.querySelector("#twoDay").innerHTML = dayAbbrv(2);
-    let iconTwoCode = response.list[10].weather[0].icon;
-    let iconTwoUrl = "http://openweathermap.org/img/w/" + iconTwoCode + ".png";
-    twoTemp.innerHTML = response.list[10].main.temp.toFixed(0);
-    twoIcon.src = iconTwoUrl;
-    twoDesc.innerHTML = response.list[10].weather[0].description;
-    // day 3
-    document.querySelector("#threeDay").innerHTML = dayAbbrv(3);
-    let iconThreeCode = response.list[18].weather[0].icon;
-    let iconThreeUrl =
-      "http://openweathermap.org/img/w/" + iconThreeCode + ".png";
-    threeTemp.innerHTML = response.list[18].main.temp.toFixed(0);
-    threeIcon.src = iconThreeUrl;
-    threeDesc.innerHTML = response.list[18].weather[0].description;
-    // day 4
-    document.querySelector("#fourDay").innerHTML = dayAbbrv(4);
-    let iconFourCode = response.list[26].weather[0].icon;
-    let iconFourUrl =
-      "http://openweathermap.org/img/w/" + iconFourCode + ".png";
-    fourTemp.innerHTML = response.list[26].main.temp.toFixed(0);
-    fourIcon.src = iconFourUrl;
-    fourDesc.innerHTML = response.list[26].weather[0].description;
-    // day 5
-    document.querySelector("#fiveDay").innerHTML = dayAbbrv(5);
-    let iconFiveCode = response.list[34].weather[0].icon;
-    let iconFiveUrl =
-      "http://openweathermap.org/img/w/" + iconFiveCode + ".png";
-    fiveTemp.innerHTML = response.list[34].main.temp.toFixed(0) + " degrees";
-    fiveIcon.src = iconFiveUrl;
-    fiveDesc.innerHTML = response.list[34].weather[0].description;
-  })
-  .catch(function(err) {
-    console.log(err);
-  });
+window.ready(weather());
