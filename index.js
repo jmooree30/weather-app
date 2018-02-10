01ae7baabc38d7a3e100de2021d78b8bb5871d48
Jacob <jmooree30@gmail.com>
2018-02-09 22:47:17 -0500

    styling

--- index.js
+++ index.js
@@ -20,6 +20,41 @@ let fiveTemp = document.querySelector("#fiveTemp");
 let fiveDesc = document.querySelector("#fiveDesc");
 let body = document.querySelector("body");
 
+function dayAbbrv(day) {
+  var d = new Date();
+  var n = d.getDay();
+  future = day + n;
+  switch (future) {
+    case 0:
+    case 7:
+      day = "Sun";
+      break;
+    case 1:
+    case 8:
+      day = "Mon";
+      break;
+    case 2:
+    case 9:
+      day = "Tue";
+      break;
+    case 3:
+    case 10:
+      day = "Wed";
+      break;
+    case 4:
+    case 11:
+      day = "Thur";
+      break;
+    case 5:
+    case 12:
+      day = "Fri";
+      break;
+    case 6:
+      day = "Sat";
+  }
+  return day
+}
+
 // current weather
 fetch(
   "https://api.openweathermap.org/data/2.5/weather?zip=02149,us&units=imperial&APPID=38c7a3bf4046499d8be4a15128e0d64a",
@@ -38,7 +73,7 @@ fetch(
     let iconCode = response.weather[0].icon;
     let iconUrl = "http://openweathermap.org/img/w/" + iconCode + ".png";
     currentIcon.src = iconUrl;
-    currentTemp.innerHTML = response.main.temp.toFixed(0) + " degrees";
+    currentTemp.innerHTML = response.main.temp.toFixed(0) + " F";
     currentDesc.innerHTML = response.weather[0].description;
     currentHumidity.innerHTML = "Humidity: " + response.main.humidity;
     currentLocation.innerHTML = response.name;
@@ -57,32 +92,37 @@ fetch(
   })
   .then(function(response) {
     // day 1
+    document.querySelector("#oneDay").innerHTML = dayAbbrv(1);
     let iconOneCode = response.list[2].weather[0].icon;
     let iconOneUrl = "http://openweathermap.org/img/w/" + iconOneCode + ".png";
-    oneTemp.innerHTML = response.list[2].main.temp.toFixed(0) + " degrees";
+    oneTemp.innerHTML = response.list[2].main.temp.toFixed(0);
     oneIcon.src = iconOneUrl;
     oneDesc.innerHTML = response.list[2].weather[0].description;
     // day 2
+    document.querySelector("#twoDay").innerHTML = dayAbbrv(2);
     let iconTwoCode = response.list[10].weather[0].icon;
     let iconTwoUrl = "http://openweathermap.org/img/w/" + iconTwoCode + ".png";
-    twoTemp.innerHTML = response.list[10].main.temp.toFixed(0) + " degrees";
+    twoTemp.innerHTML = response.list[10].main.temp.toFixed(0);
     twoIcon.src = iconTwoUrl;
     twoDesc.innerHTML = response.list[10].weather[0].description;
     // day 3
+    document.querySelector("#threeDay").innerHTML = dayAbbrv(3);
     let iconThreeCode = response.list[18].weather[0].icon;
     let iconThreeUrl =
       "http://openweathermap.org/img/w/" + iconThreeCode + ".png";
-    threeTemp.innerHTML = response.list[18].main.temp.toFixed(0) + " degrees";
+    threeTemp.innerHTML = response.list[18].main.temp.toFixed(0);
     threeIcon.src = iconThreeUrl;
     threeDesc.innerHTML = response.list[18].weather[0].description;
     // day 4
+    document.querySelector("#fourDay").innerHTML = dayAbbrv(4);
     let iconFourCode = response.list[26].weather[0].icon;
     let iconFourUrl =
       "http://openweathermap.org/img/w/" + iconFourCode + ".png";
-    fourTemp.innerHTML = response.list[26].main.temp.toFixed(0) + " degrees";
+    fourTemp.innerHTML = response.list[26].main.temp.toFixed(0);
     fourIcon.src = iconFourUrl;
     fourDesc.innerHTML = response.list[26].weather[0].description;
     // day 5
+    document.querySelector("#fiveDay").innerHTML = dayAbbrv(5);
     let iconFiveCode = response.list[34].weather[0].icon;
     let iconFiveUrl =
       "http://openweathermap.org/img/w/" + iconFiveCode + ".png";
